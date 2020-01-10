6e1ff8a50198caaec82ecd21504e292792392c7e
dayeondev <dayeon.dev@gmail.com>
2020-01-10 10:00:07 +0900

    Add types.py

---
+++ connect5/types.py
@@ -0,0 +1,12 @@
+import enum
+from collections import namedtuple
+
+
+class Player(enum.Enum):
+    black = 1
+    white = 2
+
+
+    @property
+    def other(self):
+        return Player.black if self == Player.white else Player.white
